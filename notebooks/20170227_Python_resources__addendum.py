6b4e569f6566413ac58a83850985a5ef17d20eeb
Matej Mojzes <matejmojzes@gmail.com>
2017-02-27 21:03:37 +0100

    Addendum on learning resources

---
+++ notebooks/20170227_Python_resources__addendum.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# # Resorces for learning Python, numpy, pandas and Jupyter notebooks (addendum)
+
+# **Read this guide/tutorial first**: [Running Jupyter Notebooks With The Anaconda Python Distribution](https://www.datacamp.com/community/tutorials/tutorial-jupyter-notebook).
+# 
+# Notes:
+# * You can skip parts on pip & docker. 
+# * Install Anaconda (with Python 3) only.
+# * Do not hesitate to follow links included in the guide.
+
+# ### Other resources:
+# 
+# * [Intro to Python for Data Science](https://www.datacamp.com/courses/intro-to-python-for-data-science) - I do not have personal experience with this course, but since it was referenced in the DataCamp guide, it should be OK (and free)
+# * [Python Data Science Cookbook](https://www.packtpub.com/big-data-and-business-intelligence/python-data-science-cookbook) - book I can highly recommend
+# * [Python for Data Science and Machine Learning Bootcamp](https://www.udemy.com/python-for-data-science-and-machine-learning-bootcamp/) - Udemy course, great course with videos and examples in Jupyter notebooks (often in sale for ca. $10, but not now, unfortunately)
+# * Last, but not least, **Google**. Feel free to type name of module you are interested in and you will find lots of helpful resources. Official documentation sites (e.g. for [NumPy](https://docs.scipy.org/doc/numpy-1.12.0/reference/)) are great places to start as well.
+
+# ### Good luck!!
+# 
+# Little bit of motivation:
+# 
+# <img src="img/matlab_vs_python.png">
+# 
+# Source: [Google Trends](https://trends.google.com/trends/explore?q=%2Fm%2F053_x,Python)
