62282631fadca6af1081c8d7774ab5f48edbeae5
Matej Mojzes <matejmojzes@gmail.com>
2017-03-18 23:40:16 +0100

    Fast Simulated Annealing and general ability to log various parameters during optimization

--- src/heur.py
+++ src/heur.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 class StopCriterion(Exception):
@@ -17,6 +18,7 @@ class Heuristic:
         self.best_y = np.inf
         self.best_x = None
         self.neval = 0
+        self.log_data = []
 
     def evaluate(self, x):
         y = self.of.evaluate(x)
@@ -30,11 +32,15 @@ class Heuristic:
             raise StopCriterion('Exhausted maximum allowed number of evaluations')
         return y
 
+    def log(self, data):
+        self.log_data.append(data)
+
     def report_end(self):
         return {
             'best_y': self.best_y,
             'best_x': self.best_x,
-            'neval': self.neval if self.best_y <= self.of.get_fstar() else np.inf
+            'neval': self.neval if self.best_y <= self.of.get_fstar() else np.inf,
+            'log_data': pd.DataFrame(self.log_data)
         }
 
 
@@ -82,3 +88,50 @@ class ShootAndGo(Heuristic):
             return self.report_end()
         except:
             raise
+
+
+class FastSimulatedAnnealing(Heuristic):
+
+    def __init__(self, of, maxeval, T0, n0, alpha, r):
+        Heuristic.__init__(self, of, maxeval)
+
+        self.T0 = T0
+        self.n0 = n0
+        self.alpha = alpha
+        self.r = r
+
+    def mutate(self, x):
+        # Discrete Cauchy mutation
+        n = np.size(x)
+        u = np.random.uniform(low=0.0, high=1.0, size=n)
+        r = self.r
+        x_new = x + r*np.tan(np.pi * (u-1/2))
+
+        x_new_corrected = np.minimum(np.maximum(x_new, self.of.a), self.of.b)  # trivial mutation correction (for now)
+        return np.array(np.round(x_new_corrected), dtype=int)
+
+    def search(self):
+        try:
+            x = self.of.generate_point()
+            f_x = self.evaluate(x)
+            while True:
+                k = self.neval
+                T0 = self.T0
+                n0 = self.n0
+                alpha = self.alpha
+                T = T0 / (1 + (k / n0) ** alpha) if alpha > 0 else T0 * np.exp(-(k / n0) ** -alpha)
+
+                y = self.mutate(x)
+                f_y = self.evaluate(y)
+                s = (f_x - f_y)/T
+                swap = np.random.uniform() < 1/2 + np.arctan(s)/np.pi
+                Heuristic.log(self, {'step': k, 'x': x, 'f_x': f_x, 'y': y, 'f_y': f_y, 'T': T, 'swap': swap})
+                if swap:
+                    x = y
+                    f_x = f_y
+
+        except StopCriterion:
+            return self.report_end()
+        except:
+            raise
+
