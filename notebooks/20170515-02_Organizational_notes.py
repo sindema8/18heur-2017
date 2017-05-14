54ac56d9201e0535a3849463a9580e07851d2acb
Matej Mojzes <matejmojzes@gmail.com>
2017-05-14 17:33:49 +0200

    Organizational notes

---
+++ notebooks/20170515-02_Organizational_notes.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# # Organizational notes
+# 
+# **IMPORTANT**: Please refer to `notebooks/20170220_Introduction.ipynb` for the most important information on _zápočet_.
+
+# ## Recommended topics for your own work
+# 
+# ### 1. Tuning of implemented heuristics on implemented objective functions
+# 
+# It is absolutely OK to re-use any materials from the classes, including source codes. But beware: simple re-running of existing heuristics and obj. functions, even on larger parameter space, is **not enough**.  Large scale experiments should be accompanied by reasonable analysis of the results and thoughtful conclusions!
+# 
+# ### 2. Implementation and tuning of new heuristics and/or new objective functions
+# 
+# Reasonable study of heuristic/obj. function parameters, including comparison with selected referential methods, will be required as output.
+# 
+# ### 3. Improvement of existing code base
+# 
+# By implementing unit tests, better handling of edge cases, improving documentation, packaging etc. Any work in this regard should be discussed in advance.
+
+# ## How to submit your work
+# 
+# As a Jupyter notebook (including .IPYNB, .HTML and .PY version), ideally. Regular PDF is OK as well. Just name the files reasonably (`HEURISTIKY.pdf` is a bad name) and any form readable on a mobile device will be appreciated.
+# 
+# Furthermore, consider the following technical ways of submission:
+# 
+# 1. [Pull request](https://help.github.com/articles/about-pull-requests/) is definitely the favored alternative
+# 2. Via e-mail
+# 
+# Last, but not least, English language is preferred.
+
+# ## Schedule for the rest of semester
+# 
+# TBD.
